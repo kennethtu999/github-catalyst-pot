867d31873f197cc1f4b48f5c9b9d5606937c08e7
Kenneth Tu <kenneth.tu@gmail.com>
2020-12-03 16:08:44 +0800

    first commit

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import { controller, target } from "@github/catalyst"
+
+@controller
+class HelloWorldElement extends HTMLElement {
+  @target output: HTMLElement
+
+  greet() {
+    this.output.textContent = `Hello, world!`
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require( 'path' );
+
+module.exports = {
+    // entry files
+    entry: './src/index.ts',
+
+    devtool: "source-map",
+
+    // output bundles (location)
+    output: {
+        path: path.resolve( __dirname, 'dist' ),
+        filename: 'bundle.js',
+    },
+
+    // file resolutions
+    resolve: {
+        extensions: [ '.ts', '.js' ],
+    },
+
+    // loaders
+    module: {
+        rules: [
+            {
+                test: /\.tsx?/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+              test: /\.m?js$/,
+              exclude: /(node_modules|bower_components)/,
+              use: {
+                loader: 'babel-loader',
+                options: {
+                  presets: ['@babel/preset-env'],
+                  plugins: ['transform-custom-element-classes', 'transform-es2015-classes']
+                }
+              }
+            }
+        ]
+    }
+};
